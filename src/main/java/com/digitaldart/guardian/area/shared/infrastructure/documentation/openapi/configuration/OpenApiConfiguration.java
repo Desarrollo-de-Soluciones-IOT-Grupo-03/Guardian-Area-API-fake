9b5c59819508f39ef9d71f627435636e7c7d2c48
Cristian Quito <cristianandresquito@gmail.com>
2024-10-06 20:09:55 -0500

    initial commit

---
+++ src/main/java/com/digitaldart/guardian/area/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
@@ -0,0 +1,9 @@
+package com.digitaldart.guardian.area.shared.infrastructure.documentation.openapi.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfiguration {
+
+
+}

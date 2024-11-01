5c7f2c0d13ae43f34ee1720baefbc7660ca3fffe
Cristian Quito <cristianandresquito@gmail.com>
2024-11-01 13:05:13 -0500

    feat: add health-measures-stream

--- src/main/java/com/digitaldart/guardian/area/monitoring/infrastructure/persistence/jpa/repositories/DeviceRepository.java
+++ src/main/java/com/digitaldart/guardian/area/monitoring/infrastructure/persistence/jpa/repositories/DeviceRepository.java
@@ -1,6 +1,7 @@
 package com.digitaldart.guardian.area.monitoring.infrastructure.persistence.jpa.repositories;
 
 import com.digitaldart.guardian.area.monitoring.domain.model.aggregates.Device;
+import com.digitaldart.guardian.area.monitoring.domain.model.valueobjects.ApiKey;
 import com.digitaldart.guardian.area.monitoring.domain.model.valueobjects.GuardianAreaDeviceRecordId;
 import com.digitaldart.guardian.area.monitoring.domain.model.valueobjects.UserId;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -14,4 +15,5 @@ public interface DeviceRepository extends JpaRepository<Device, Long> {
     List<Device> findAllByUserId(UserId userId);
     Optional<Device> findByGuardianAreaDeviceRecordId(GuardianAreaDeviceRecordId guardianAreaDeviceRecordId);
     boolean existsByGuardianAreaDeviceRecordId(GuardianAreaDeviceRecordId guardianAreaDeviceRecordId);
+    Optional<Device> findByGuardianAreaDeviceRecordIdAndApiKey(GuardianAreaDeviceRecordId guardianAreaDeviceRecordId, ApiKey apiKey);
 }
